bd5abf9d31169477a6fb0cd49aa7067c7cc345b4
KateKucheruk <ki290489kea@gmail.com>
2024-07-28 17:15:08 +0300

    add abstract five

---
+++ js/abstrsct-five.js
@@ -0,0 +1,6 @@
+const sectionFirst = document.querySelector(`.js-list`);
+
+console.dir(sectionFirst);
+console.log(sectionFirst.children);
+console.log(sectionFirst.childNodes);
+console.log(sectionFirst.parentNode);
